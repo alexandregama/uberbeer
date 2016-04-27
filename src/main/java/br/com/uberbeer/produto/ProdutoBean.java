77fad9cb6a74eace973c6ff87a7d0c838ed73a37
Alexandre Gama <alexandre.gama.lima@gmail.com>
2016-04-27 20:09:02 -0300

    Refatora a listagem de produtos para nao buscar do banco toda vez que o metodo eh chamado

--- src/main/java/br/com/uberbeer/produto/ProdutoBean.java
+++ src/main/java/br/com/uberbeer/produto/ProdutoBean.java
@@ -2,6 +2,8 @@ package br.com.uberbeer.produto;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean
@@ -9,15 +11,32 @@ public class ProdutoBean {
 
 	private Produto produto = new Produto();
 	
+	private List<Produto> produtos;
+	
+	public ProdutoBean() {
+		System.out.println("Construindo ProdutoBean..");
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("ProdutoBean construido");
+	}
+	
 	public void gravar() {
 		ProdutoDao dao = new ProdutoDao();
 		dao.salva(produto);
 		produto = new Produto();
+		System.out.println("Produto gravado com sucesso!");
 	}
 
 	public List<Produto> getProdutos() {
-		ProdutoDao dao = new ProdutoDao();
-		return dao.listaTodos(); 
+		System.out.println("Listando produtos..");
+		if (produtos == null) {
+			System.out.println("Lista de produtos vazia. Recuperando novamente..");
+			ProdutoDao dao = new ProdutoDao();
+			produtos = dao.listaTodos();
+		}
+		return produtos; 
 	}
 	
 	public Produto getProduto() {
@@ -27,5 +46,9 @@ public class ProdutoBean {
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
-	
+
+	@PreDestroy
+	public void destroy() {
+		System.out.println("Destruindo ProdutoBean..");
+	}
 }
