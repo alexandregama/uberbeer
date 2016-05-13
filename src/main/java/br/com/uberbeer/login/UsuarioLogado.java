8dc6feda73f62f3c38d37963f7f38c5f2cf576dc
Alexandre Gama <alexandre.gama.lima@gmail.com>
2016-05-13 18:40:40 -0300

    Adiciona classe de UsuarioLogado esquecida no PR anterior

---
+++ src/main/java/br/com/uberbeer/login/UsuarioLogado.java
@@ -0,0 +1,34 @@
+package br.com.uberbeer.login;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import br.com.uberbeer.usuario.Usuario;
+
+@SessionScoped
+@Named
+public class UsuarioLogado implements Serializable {
+
+	private static final long serialVersionUID = 173266633307427002L;
+	
+	private Usuario usuario;
+
+	public void loga(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public boolean isLogged() {
+		return this.usuario != null;
+	}
+	
+	public void logout() {
+		this.usuario = null;
+	}
+	
+	public Usuario getUsuario() {
+		return this.usuario;
+	}
+	
+}
